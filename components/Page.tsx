ca9306329d65480a85576304d67058fc7cd90c11
Rok Strnisa <rok.strnisa@gmail.com>
2025-09-22 09:23:40 +0100

    Improve layout on iOS.

--- components/Page.tsx
+++ components/Page.tsx
@@ -11,7 +11,7 @@ export default function Page(props: Props): JSX.Element {
     const { children } = props;
 
     return (
-        <div className="flex h-screen flex-col">
+        <div className="flex h-[100dvh] flex-col">
             <Header />
             <div className="flex min-h-0 w-full flex-1 flex-col items-center justify-center px-2">{children}</div>
             <Footer />
